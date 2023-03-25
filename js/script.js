6e22de091fff0d79f7b4c51d3fd2d562995bc798
sultonakobirov <zafarzhanovich@gmail.com>
2023-03-25 11:01:34 +0500

    "weff"sdfs"

--- js/script.js
+++ js/script.js
@@ -236,19 +236,20 @@ let website = {
   net: [],
   info: [],
   com: []}
-let other = []
+// let other = []
+// users= users.forEach(item => {
+//     console.log(item.website);
+// })
 
-website.net = users.filter(item => item.website.includes(".net") == true)
-website.org = users.filter(item => item.website.includes(".org") == true)
-website.info = users.filter(item => item.website.includes(".info") == true)
-website.com = users.filter(item => item.website.includes(".com") == true)
+website.net = users.filter(item => item.includes(".net") == true)
+website.org = users.filter(item => item.includes(".org") == true)
+website.info = users.filter(item => item.includes(".info") == true)
+website.com = users.filter(item => item.includes(".com") == true)
 
-other = users.filter(item => item.website.includes(".net") == false && item.website.includes(".org") == false && item.website.includes(".info") == false && item.website.includes(".com") == false) // фильтруем элементы которые не равны com net org in
-other = other.filter(element => {
-    console.log(element.website);
-})
-// console.log(other);
-// console.log(website);
+other = users.filter(item => item.includes(".net") == false && item.includes(".org") == false && item.includes(".info") == false && item.includes(".com") == false) // фильтруем элементы которые не равны com net org in
+
+console.log(other);
+console.log(website);
 
 
 
