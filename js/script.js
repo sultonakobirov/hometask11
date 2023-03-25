47e1f4d264872f6f7038480432e4da5704ea9ec1
Sulton Akobirov <102214317+sultonakobirov@users.noreply.github.com>
2023-03-25 11:07:44 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -231,6 +231,7 @@ let users = [
     }
   }
 ]
+
 let website = {
   org: [],
   net: [],
@@ -238,18 +239,18 @@ let website = {
   com: []}
 let other = []
 
-// let other = []
-// users= users.forEach(item => {
-//     console.log(item.website);
-// })
+users = users.forEach(item => {
+    item.splice(users.lastIndexOf('website'), 1)
+    console.log(users);
+})
+website.net = users.filter(item => item.website.includes(".net") == true)
+website.org = users.filter(item => item.website.includes(".org") == true)
+website.info = users.filter(item => item.website.includes(".info") == true)
+website.com = users.filter(item => item.website.includes(".com") == true)
+website = website.forEach(item => { 
 
-website.net = users.filter(item => item.includes(".net") == true)
-website.org = users.filter(item => item.includes(".org") == true)
-website.info = users.filter(item => item.includes(".info") == true)
-website.com = users.filter(item => item.includes(".com") == true)
-
-other = users.filter(item => item.includes(".net") == false && item.includes(".org") == false && item.includes(".info") == false && item.includes(".com") == false) // фильтруем элементы которые не равны com net org in
-
+    });
+other = users.filter(item => item.website.includes(".net") == false && item.website.includes(".org") == false && item.website.includes(".info") == false && item.website.includes(".com") == false) // фильтруем элементы которые не равны com net org in
 console.log(other);
 console.log(website);
 
