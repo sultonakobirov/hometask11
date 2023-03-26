35e9e478777eaf61c59c715c93da85385c790c67
sultonakobirov <zafarzhanovich@gmail.com>
2023-03-26 15:07:58 +0500

    debug

--- js/script.js
+++ js/script.js
@@ -236,21 +236,15 @@ let website = {
   net: [],
   info: [],
   com: []}
-// let other = []
-// users= users.forEach(item => {
-//     console.log(item.website);
-// })
+let other = []
 
-website.net = users.filter(item => item.includes(".net") == true)
-website.org = users.filter(item => item.includes(".org") == true)
-website.info = users.filter(item => item.includes(".info") == true)
-website.com = users.filter(item => item.includes(".com") == true)
 
-other = users.filter(item => item.includes(".net") == false && item.includes(".org") == false && item.includes(".info") == false && item.includes(".com") == false) // фильтруем элементы которые не равны com net org in
+website.net = users.filter(item => item.website.includes(".net") == true)
+website.org = users.filter(item => item.website.includes(".org") == true)
+website.info = users.filter(item => item.website.includes(".info") == true)
+website.com = users.filter(item => item.website.includes(".com") == true)
+other = users.filter(item => item.website.includes(".net") == false && item.website.includes(".org") == false && item.website.includes(".info") == false && item.website.includes(".com") == false) // фильтруем элементы которые не равны com net org in
 
-console.log(other);
 console.log(website);
-
-
-
+console.log(other);
 
